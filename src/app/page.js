8380e103b329d7ff6976f515981d72f06acfa840
Kavya Mukhija <mukhijakavya02@gmail.com>
2025-11-17 12:23:27 +0530

    done

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
-  const router = useRouter
+  const router = useRouter()
   useEffect(()=>{
     router.push("/login")
   },[])
